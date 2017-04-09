200a0053deb807854a7a10c8359bb8f353de5d51
Kovcheq <Kovcheq@gmail.com>
2017-04-09 23:06:50 +0300

    Add _autoload

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
-// FRONT COTROLLER
+    // FRONT COTROLLER
 
-// 1. Общие настройки
+    // Общие настройки
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-// 2. Подключение файлов системы
+    session_start();
 
-define('ROOT', dirname(__FILE__));
-require_once(ROOT.'/components/Router.php');
-require_once(ROOT.'/components/Db.php');
+    // Подключение файлов системы
 
-// 3. Установка соединения с БД
+    define('ROOT', dirname(__FILE__));
+    require_once(ROOT.'/components/Autoload.php');
 
 
-// 4. Вызор Router
+    // Вызор Router
 
-$router = new Router();
-$router->run();
+    $router = new Router();
+    $router->run();
